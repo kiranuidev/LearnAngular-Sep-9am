6eef2a1e1592ae8477eb0da4c733ec8f7adb1c0a
kiran paturi <kpaturi@smarttuition.com>
2016-09-19 09:51:01 +0530

    added basic js code

---
+++ JsBasics/firstExample.js
@@ -0,0 +1,24 @@
+//type casts i to integer.
+
+/*
+var i=10 casts the value of i to an integer
+var iAgree = false; casts the value of iAgree to boolean
+var firstName = "Kiran"; casts the value of firstName to string
+var users = ["kiran", "ravi", 1, false]
+casts the value of users to an array
+decimal
+double
+float
+
+*/
+'use strict';
+i = 10;
+iAgree = false;
+firstName = "Kiran";
+users = ["kiran", "ravi", 1, false];
+console.log(i);
+person = {}; //object literal
+person.FirstName = "kiran";
+person.age = 30;
+person.gender = "M";
+console.log(person);
