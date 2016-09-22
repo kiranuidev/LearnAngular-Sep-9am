bee1fec87449f3ca4d8be53f8891e0f07db70597
kiranuidev <trainer.paturi@gmail.com>
2016-09-22 10:08:36 -0700

    added code

---
+++ Register/register.js
@@ -0,0 +1,49 @@
+var controls = {};
+controls.firstName = document.getElementById("txtFirstName");
+controls.lastName = document.getElementById("txtLastName");
+controls.registerButton = document.getElementById("btnRegister");
+
+function getValuesFromControls(){
+    var userDetails={};
+    userDetails.firstName=controls.firstName.value;
+    userDetails.lastName=controls.lastName.value;
+}
+
+function registerUser(){
+    alert("Hey User clicked");
+}
+
+function alphabetsOnly(evt){
+    //console.log(evt);
+    if((evt.keyCode>=97 && evt.keyCode<=122)
+       ||(evt.keyCode>=65 && evt.keyCode<=90)
+       ||(evt.keyCode==32 ||evt.keyCode==46)
+      ){
+        
+    }
+    else{
+        evt.preventDefault();
+    }
+}
+
+function bindEventsToControls(){
+ controls.registerButton.addEventListener('click',registerUser);
+    controls.firstName.addEventListener("keypress",alphabetsOnly);
+    controls.lastName.addEventListener("keypress",alphabetsOnly);
+     /*controls.registerButton.addEventListener('mouseover',registerUser);*/
+}
+
+bindEventsToControls();
+
+
+
+
+
+
+
+
+
+
+
+
+
