d0b313733507fb1a46da9a9a798794d1cc843f4c
kiranpaturi <kiran_p@k3informatics.com>
2016-10-27 10:03:05 +0530

    added code

--- AngularApp/app/header/headerCtrl.js
+++ AngularApp/app/header/headerCtrl.js
@@ -1,5 +1,6 @@
 (function () {
     function headerCtrl($scope, lookupSvc) {
+        $scope.cartItems = 0;
         //$scope.navItems =["Home","Login","Register"];
         $scope.navItems = lookupSvc.getNavItems();
         $scope.brandName = "FlipSnap";
@@ -10,7 +11,19 @@
             // console.log(kiran);
             $scope.contentUrl = data.templateUrl;
         };
-    }
+
+        $scope.$on("ITEM_ADDED", function (evt, args) {
+            console.log(args);
+            $scope.cartItems++;
+        });
+
+        $scope.$on("ITEM_REMOVED", function (evt, args) {
+            console.log(args);
+            $scope.cartItems--;
+        });
+
+
+    };
 
 
     angular.module("header").controller("headerCtrl", headerCtrl);
